3399312839b8f87be3cef5b3f1b42703520e8d50
Pubec929 <pubec.308@gmail.com>
2026-07-09 23:19:33 +0200

    rebuild utils color helper

---
+++ src/utils/colors.py
@@ -0,0 +1,22 @@
+
+class ColorMap:
+    RED = "#FF0000"          
+    GREEN = "#00FF00"       
+    ORANGE = "#FFAF00"       
+    BUY = "#3A96DD"          
+    SELL = "#B4009E"         
+
+class MarketColorMap:
+    ethereum = "#005FFF"     
+    bitcoin = "#FFAF00"      
+    solana = "#5F00D7"       
+    xrp = "#0087FF"   
+    default = ""       
+
+def getMarketColor(market_name: str):
+    market_name = market_name.lower()
+    for market in MarketColorMap.__dict__:
+        if market in market_name:
+            return getattr(MarketColorMap, market)
+
+    return ""
